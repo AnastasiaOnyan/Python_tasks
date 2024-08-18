66fd5c88d11246b2d507550c825cde319f3dd9fe
Anastasia Onyan <anapaul@yandex.ru>
2024-08-18 20:32:15 +0300

    add new func to the file

--- combinatorics_recursion.py
+++ combinatorics_recursion.py
@@ -1,4 +1,4 @@
-#calculate unique combinations (1, 2 == 2, 1) where n is an amount of all elements which are given and k - the amount of elements in each combination
+#1) calculate unique combinations (1, 2 == 2, 1) where n is an amount of all elements which are given and k - the amount of elements in each combination
 
 first = int(input("Enter a number: "))
 second = int(input("Enter a number: "))
@@ -20,3 +20,15 @@ def partly_factorial(n, k): #factorial from k to n
 unique_combs = partly_factorial(first, second + 1)/factorial(first - second)
 print(unique_combs)
 
+
+
+
+
+#2)A recursive function in Python. Consider the following function power(), which takes in two integers x and n and returns the value x^n (x in power n)
+
+
+def power(x, n):
+    if n == 0:
+        return 1
+    else:
+        return power(x, n - 1) * x
